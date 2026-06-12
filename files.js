2550f0fc868dad4d1c518b12870ede18f0c9b9a7
Uwakmfon1 <udohuwakmfon2@gmail.com>
2026-06-12 12:34:19 +0100

    added writeFile and check if FileExists

--- files.js
+++ files.js
@@ -1,5 +1,29 @@
 const fs = require('fs');
 
-fs.readFile('./assets/docs/blog1.txt', 'utf-8', (err, data)=>{
-    console.log(data);
+// fs.readFile('./assets/docs/blog1.txt', 'utf-8', (err, data)=>{
+//     console.log(data.toString());
+// });
+
+
+fs.writeFile('./assets/docs/blog2.txt', 'hello world, welcome to another node js tutorial', (err)=>{
+    if(err){
+        console.log(err);
+    }      
+    console.log('file written successfully');        
 });
+
+if(!fs.existsSync('./prod')){
+    fs.mkdir('./prod', (err)=>{
+        if(err){
+            console.log(err);
+        }
+    });
+    console.log('folder created');
+}else{
+    fs.rmdir('./prod',((err)=>{
+        if(err){
+            console.log(err);
+        }
+    }))
+    console.log('folder deleted');
+}
